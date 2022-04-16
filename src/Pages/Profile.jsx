7519c4156f8c05eab00587069b536ad9a52dd694
Riya Chapatwala <chapatwalariya@google.com>
2022-04-17 00:53:46 +0530

    profile(abouts us, contact us)

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, Image, Link, Text } from "@chakra-ui/react";
 import React from "react";
 import Layout from "../Component/Layout";
 import name from "../Images/name.svg";
@@ -9,48 +9,81 @@ import chat from "../Images/chat.svg";
 import service from "../Images/service.svg";
 import signout from "../Images/signout.svg";
 import share from "../Images/share.svg";
+import subscription from "../Images/subscription.svg";
+import pricing from "../Images/pricing.svg";
+import { font14, font500, lightblue, roboto } from "../Constant";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Profile = () => {
+  const history = useHistory();
   return (
     <Box w="100%">
       <Layout card={true} />
-      <Box p="5">
-        <Flex>
-          <Image src={name} />
+      <Box
+        px="33px"
+        py="5"
+        fontFamily={roboto}
+        fontWeight={font500}
+        fontSize={font14}
+      >
+        <Flex
+          cursor="pointer"
+          onClick={() => history.push("/create-profile", { edit: true })}
+        >
+          <Image mr="12px" src={name} />
           Edit Profile
         </Flex>
-        <Flex>
-          <Image src={business} />
+        <Flex mt="20px">
+          <Image mr="12px" src={business} />
           Edit Business
         </Flex>
-        <Flex>
-          <Image src={money} />
+        <Flex mt="20px">
+          <Image mr="12px" src={money} />
           Total Earning
         </Flex>
-        <Flex>
-          <Image src={money} />
+        <Flex mt="20px">
+          <Image mr="12px" src={subscription} />
           Subscription
         </Flex>
-        <Flex>
-          <Image src={aboutus} />
+        <Flex mt="20px">
+          <Image mr="12px" src={pricing} />
+          Our Pricing
+        </Flex>
+        <Flex
+          cursor="pointer"
+          mt="20px"
+          onClick={() => history.push("/aboutus")}
+        >
+          <Image mr="12px" src={aboutus} />
           About Us
         </Flex>
-        <Flex>
-          <Image src={service} />
+        <Flex mt="20px">
+          <Image mr="12px" src={service} />
           Terms Of Services
         </Flex>
-        <Flex>
-          <Image src={chat} />
-          Support Chat
-        </Flex>
-        <Flex>
-          <Image src={share} />
+        <Link
+          textDecoration="none"
+          href="https://chat.whatsapp.com/L7v4LPgQQ1sAZ0tsQ3XqpU"
+        >
+          <Flex mt="20px" cursoe="pointer">
+            <Image mr="12px" src={chat} />
+            Support Chat
+          </Flex>
+        </Link>
+        <Flex mt="20px">
+          <Image mr="12px" src={share} />
           Invite Your Friends To Drop
         </Flex>
-        <Flex>
-          <Image src={signout} />
+        <Flex mt="20px">
+          <Image mr="12px" src={signout} />
           Signout
         </Flex>
+        <Text color="#001833" textAlign="center" mt="5">
+          drop by{" "}
+          <Link to="https://drop-pwa.netlify.app" color={lightblue}>
+            ZOGNEST.
+          </Link>
+        </Text>
       </Box>
     </Box>
   );
